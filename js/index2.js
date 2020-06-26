ce2f96a72afee662e997d7d87c33984ef5069092
juan antonio rosales <juanantoniorosales@iMac-de-juan.local>
2020-06-26 11:27:24 +0200

    DragDrop shop

--- js/index2.js
+++ js/index2.js
@@ -36,7 +36,7 @@ function drop(ev) {
 function addEvent(element, event, delegate) {
   if (typeof window.event != "undefined") {
     element.attachEvent("on" + event, delegate);
-  } /*  else {
+  } /* else {
     element.addEventListener(event, delegate, false);
   } */
 }
@@ -45,16 +45,24 @@ addEvent(document, "readystatechange", function () {
   if (document.readyState !== "complete") return true;
 });
 
-var items = document.querySelectorAll("div.productos-productoTop div img");
-var cart = document.querySelectorAll("section#cart ul")[0];
+let items = document.querySelectorAll("div.drag_item img");
+let cart = document.querySelectorAll("section#cart ul")[0];
 
 function updateCart() {
   var total = 0.0;
   var cart_items = document.querySelectorAll("#cart ul li");
+  console.log("Este es el cart_items ");
+  console.log(cart_items);
   for (var i = 0; i < cart_items.length; i++) {
     var cart_item = cart_items[i];
+    console.log("Este es el cart_item ");
+    console.log(cart_item);
     var quantity = cart_item.getAttribute("data-quantity");
+    console.log("Este es el quantity ");
+    console.log(quantity);
     var price = cart_item.getAttribute("data-price");
+    console.log("Este es el price ");
+    console.log(price);
 
     var sub_total = parseFloat(quantity * parseFloat(price));
     cart_item.querySelectorAll("span.sub-total")[0].innerHTML =
@@ -66,16 +74,22 @@ function updateCart() {
   document.querySelectorAll("#cart span.total")[0].innerHTML = total.toFixed(2);
 }
 
-function addCartItem(item, id) {
+function addCartItem(item, id, price) {
+  let cart = document.querySelectorAll("section#cart ul")[0];
+  console.log("Este es el item ");
   console.log(item);
-  console.log("Este es el id " + id);
-  var clone = item.cloneNode(true);
+  console.log("Este es el id ");
+  console.log(id);
+  var clone = document.createElement("li");
   clone.setAttribute("data-id", id);
+  clone.setAttribute("data-price", price);
   clone.setAttribute("data-quantity", 1);
   clone.removeAttribute("id");
-  console.log("Este es el clone" + clone);
+  console.log("Este es el clone ");
+  console.log(clone);
 
   var fragment = document.createElement("span");
+  console.log("Este es el fragment ");
   console.log(fragment);
   fragment.setAttribute("class", "quantity");
   fragment.innerHTML = " x 1";
@@ -84,17 +98,21 @@ function addCartItem(item, id) {
   fragment = document.createElement("span");
   fragment.setAttribute("class", "sub-total");
   clone.appendChild(fragment);
-  /* cart.appendChild(clone); */
+  console.log("Este es el cart ");
+  console.log(cart);
+  cart.appendChild(clone);
 }
 
 function updateCartItem(item) {
   var quantity = item.getAttribute("data-quantity");
 
   quantity = parseInt(quantity) + 1;
-  console.log("Este es el quantity" + quantity);
+  console.log("Este es el quantity ");
+  console.log(quantity);
 
   item.setAttribute("data-quantity", quantity);
   let span = item.querySelectorAll("span.quantity");
+  console.log("Este es el span ");
   console.log(span);
   span[0].innerHTML = " x " + quantity;
 }
@@ -106,7 +124,12 @@ function onDrop(ev) {
 
   let data = ev.dataTransfer.getData("text");
   let element = document.getElementById(data);
+  let price = element.getAttribute("data-price");
+  console.log("Este es el pricefffff ");
+  console.log(price);
+  console.log("Este es el data ");
   console.log(data);
+  console.log("Este es el element ");
   console.log(element);
 
   ev.target.appendChild(element);
@@ -115,15 +138,19 @@ function onDrop(ev) {
   element.setAttribute("draggable", "false");
 
   var exists = document.querySelectorAll(
-    "div.productos-productoTop div img[data-id='" + data + "']"
+    '#cart ul li [data-id="' + data + '"]'
   );
-  console.log("Este es el exists" + exists);
+  /* var exists = document.querySelectorAll(
+    "div.productos img[data-id='" + data + "']"
+  ); */
+  console.log("Este es el exists");
+  console.log(exists);
 
   if (exists.length > 0) {
     console.log(exists.length);
     updateCartItem(exists[0]);
   } else {
-    addCartItem(element, data);
+    addCartItem(element, data, price);
   }
 
   updateCart();
